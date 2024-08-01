b6f463e64ec7d418deac10c3b56085af3ba20c14
ONG YI REN <yiren.ong.2021@scis.smu.edu.sg>
2024-08-01 13:43:29 +0800

    Updating hybrid aircon

--- Calypso/src/views/HybridAirconSystem.vue
+++ Calypso/src/views/HybridAirconSystem.vue
@@ -67,12 +67,12 @@
         <p>If Indoor Air Quality Sensor on acceptable CO2 Level, Motorized Dampener turned off and Fresh Air Fan turn off, else both turned on.</p>
       </div>
       <div class="switch-buttons mt-4 text-center">
-        <button @click="setAllSwitches(true)" class="btn btn-primary">ON ALL</button>
-        <button @click="setAllSwitches(false)" class="btn btn-danger">OFF ALL</button>
+        <button @click="setAllSwitches(true)" class="btn btn-primary">ON ALL BUTTON</button>
+        <button @click="setAllSwitches(false)" class="btn btn-danger">OFF ALL BUTTON</button>
         <button v-for="n in 8" :key="n" @click="toggleSwitch(n)" :class="{ 'btn-success': switchStates[n - 1], 'btn-secondary': !switchStates[n - 1] }" class="btn">SWITCH {{ n }} {{ switchStates[n - 1] ? 'ON' : 'OFF' }}</button>
       </div>
       <div class="aircon-buttons mt-4 text-center">
-        <button @click="setAirconState(true, 1)" class="btn btn-primary">ON Aircons 1</button>
+        <button @click="setAirconState(true, 1)" class="btn btn-primary">ON Aircon 1</button>
         <button @click="setAirconState(false, 1)" class="btn btn-danger">OFF Aircon 1</button>
         <button @click="setAirconState(true, 2)" class="btn btn-primary">ON Aircon 2</button>
         <button @click="setAirconState(false, 2)" class="btn btn-danger">OFF Aircon 2</button>
